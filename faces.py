898976e960e0f64c9698d9215a71f2fa5f116f6a
Chris <chris.goringe@gmail.com>
2023-10-28 15:30:54 +1100

    FaceNet

--- faces.py
+++ faces.py
@@ -1,16 +1,30 @@
-import face_recognition
+from deepface import DeepFace
 import numpy as np
 import torch
 import folder_paths
 from PIL import Image
-import os
+import os, random
 
-def convert(image:torch.Tensor):
-    filepath = os.path.join(folder_paths.get_temp_directory(),"fr.png")
+DETECTORS = [ 'ssd', 'mtcnn', 'retinaface', 'mediapipe', 'opencv',  ]
+# 'dlib' seems not to download  (issue #2), 'yolov8','yunet','fastmtcnn' detect nothing (issue #3)
+# 'opencv' doesn't detect all faces (issue #6)
+
+MODELS = ["Facenet512", "Facenet", "VGG-Face", "OpenFace", "DeepFace", "Dlib"]
+# "DeepID" gives everything 0.0 (issue #5)
+
+def save_temp(image:torch.Tensor):
+    filepath = os.path.join(folder_paths.get_temp_directory(),f"{random.randint(1000000,9999999)}.png")
     image = 255. * image.cpu().numpy()
     image = Image.fromarray(np.clip(image, 0, 255).astype(np.uint8))
     image.save(filepath)
-    return face_recognition.load_image_file(filepath)
+    return filepath
+
+def similarity(file1, file2, detector, model):
+    try:
+        result = DeepFace.verify(img1_path = file1, img2_path = file2, detector_backend=detector, model_name=model)
+        return 1.0 - result['distance']
+    except ValueError:
+        return 0.0
 
 class FaceCompare:
     RETURN_TYPES = ("FLOAT","STRING",)
@@ -19,16 +33,18 @@ class FaceCompare:
     CATEGORY = "faces"
     @classmethod
     def INPUT_TYPES(s):
-        return { "required": { "image1" : ("IMAGE", {}), "image2" : ("IMAGE", {}),  }, }
+        return { "required": { 
+            "image1" : ("IMAGE", {}), 
+            "image2" : ("IMAGE", {}),  
+            "detector": (DETECTORS, {}),
+            "model" : (MODELS, {}),
+            }, }
     
-    def func(self, image1, image2):
+    def func(self, image1, image2, detector, model):
         if not image1.shape[0]==1 and image2.shape[0]==1: return (0,"Batches not supported",)
-        f1 = face_recognition.face_encodings(convert(image1[0]), model="large")
-        f2 = face_recognition.face_encodings(convert(image2[0]), model="large")
-
-        if not (len(f1)==1 and len(f2)==1): return (0,"Need one face in each image",)
-
-        s = 1.0 - face_recognition.face_distance(f1, f2[0])[0]
+        f1 = save_temp(image1[0])
+        f2 = save_temp(image2[0])
+        s = similarity(f1, f2, detector, model)
         return (s,f"{s}",)
     
 class MostSimilar:
@@ -38,22 +54,20 @@ class MostSimilar:
     CATEGORY = "faces"
     @classmethod
     def INPUT_TYPES(s):
-        return { "required": { "true_image" : ("IMAGE", {}), "candidates" : ("IMAGE", {}),  }, }
+        return { "required": { 
+            "true_image" : ("IMAGE", {}), 
+            "candidates" : ("IMAGE", {}),
+            "detector": (DETECTORS, {}),   
+            "model" : (MODELS, {}), 
+            }, }
     
-    def func(self, true_image, candidates):
+    
+    def func(self, true_image:torch.Tensor, candidates:torch.Tensor, detector, model):
         if not true_image.shape[0]==1: return (None,0,"Need a single true_image",)
-        f1 = face_recognition.face_encodings(convert(true_image[0]), model="large")
-        if len(f1)!=1: return(None,0,"Need exactly one face in true_image")
-        similarities = []
-        for i2 in candidates:
-            con = convert(i2)
-            f2 = face_recognition.face_encodings(con, model="large")
-            if len(f2)==1:
-                similarities.append((1.0-face_recognition.face_distance(f1, f2[0])[0],i2))
-            else:
-                similarities.append((0,i2))
-        
-        similarities.sort(reverse=True)
+        f1 = save_temp(true_image[0])
+
+        similarities = [(similarity(f1,save_temp(i2),detector,model),i2) for i2 in candidates]
+        similarities.sort(reverse=True, key=lambda a:a[0])
         
         return (torch.stack(tuple([s[1] for s in similarities])), f"{tuple([s[0] for s in similarities])}", )
         
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Faces
 
-A light wrapper around [ageitgey/face_recognition](https://github.com/ageitgey/face_recognition) to do facial recognition in Comfy.
+A light wrapper around [serengil/deepface](https://github.com/serengil/deepface) to do facial recognition in Comfy.
 
 ## Shameless plug for my other nodes
 
@@ -10,14 +10,14 @@ Pause the workflow while you pick an image? [Image Picker](https://github.com/ch
 
 ## Install
 
-You need to install dlib and face_recognition. This might work:
+You need to install deepface, and the default library used is facenet
 
 ```
-pip install dlib
-pip install face_recognition
+pip install deepface
+pip install facenet-pytorch
 ```
 
-If it doesn't, go to [ageitgey/face_recognition](https://github.com/ageitgey/face_recognition) and look at the instructions there.
+[Other possible dependencies](https://github.com/serengil/deepface/blob/master/requirements_additional.txt) - don't install these unless you're told to!
 
 Then install the custom node:
 
@@ -26,12 +26,14 @@ cd [comfy]/custom_nodes
 git clone https://github.com/chrisgoringe/cg-faces
 ```
 
+Deepface uses various underlying models (check their page for details) which will download on first use.
+ 
 ## Use
 
-The nodes all take a single image of the face you are comparing with, and one or more candidate images. Each candidate is compared with the `true_image`, and given a score for the similarity of the face (just the face - nothing else is taken into account). An image which doesn't have exactly one face in scores 0, a perfect match scores 1. Above 0.7 is pretty good.
+The nodes all take a single image of the face you are comparing with, and one or more candidate images. Each candidate is compared with the `true_image`, and given a score for the similarity of the face (just the face - nothing else is taken into account). An image which doesn't have a face in scores 0 (if there are multiple faces in the candidate image the best match is used) a perfect match scores 1. Above 0.6 is generally considered a match.
 
 Nodes have a `message` output which is a string, either giving some similarity numbers or an error message. Install [display text from quicknodes](https://github.com/chrisgoringe/cg-quicknodes) if you don't have a node to display a text message.
 
-Nodes either give a score, or sort the images, or return those above a threshold... they should be pretty obvious.
+Nodes either give a score, or sort the images, or return those above a threshold... usage should be pretty obvious.
 
 ![example](docs/screenshot.png)
